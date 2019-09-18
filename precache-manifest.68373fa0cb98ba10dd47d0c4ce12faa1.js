bcf71257cbc1f6744b827237125798b8c4bcf5c1
Jashele T <jashele@Jasheles-Air.domain>
2019-09-18 12:49:23 -0700

    Updates

---
+++ precache-manifest.68373fa0cb98ba10dd47d0c4ce12faa1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f84b66d7594495e12f6c0f1706f7a499",
+    "url": "/add-movies/index.html"
+  },
+  {
+    "revision": "a3fdba34bec9660f22bf",
+    "url": "/add-movies/static/css/main.91980062.chunk.css"
+  },
+  {
+    "revision": "c587c21d4046a8cc583b",
+    "url": "/add-movies/static/js/2.d19a9c7f.chunk.js"
+  },
+  {
+    "revision": "a3fdba34bec9660f22bf",
+    "url": "/add-movies/static/js/main.2eb99a9a.chunk.js"
+  },
+  {
+    "revision": "51110d5d07da76027f7e",
+    "url": "/add-movies/static/js/runtime~main.afda9e3a.js"
+  }
+]);
